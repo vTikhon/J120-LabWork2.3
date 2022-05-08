8e4eeed87e9f58487996c5ed54d1dddec46e9d59
vTikhon <tikhon.v@gmail.com>
2022-05-08 22:52:19 +0300

    map doesnt give values

--- src/ru/avalon/vergentev/j120/labwork2c/Decripter.java
+++ src/ru/avalon/vergentev/j120/labwork2c/Decripter.java
@@ -4,9 +4,9 @@ import java.util.*;
 
 public class Decripter {
     File file;
-    StringBuilder data, dataWithoutComments, temp;
-    String[] dataEachString, dataEachStringWithoutComments, dataEachStringForPrint, dataEachStringForSet, dataEachStringForSetEachFragment;
-    Properties map;
+    StringBuilder data, dataWithoutComments;
+    String[] dataEachString, dataEachStringWithoutComments, dataEachStringForPrint;
+    HashMap<String, String> map = new HashMap<>();;
 
     public Decripter (File file) {
         this.file = file;
@@ -38,63 +38,29 @@ public class Decripter {
                 dataWithoutComments.append(i).append('\n');
             }
         }
-        dataEachStringWithoutComments = dataWithoutComments.toString().split("\n");
+        dataEachStringWithoutComments = dataWithoutComments.toString().split(System.lineSeparator());
         return dataEachStringWithoutComments;
     }
 
-
-//    public void printDecriptData () {
-//        dataWithoutComments = new StringBuilder();
-//        for (String i : dataEachString) {
-//            if (!(i.charAt(0)=='#') && !(i.length() == 1)) {
-//                dataWithoutComments.append(i).append('\n');
-//            }
-//        }
-//        dataEachString = dataWithoutComments.toString().split("\n");
-//        for (String i : dataEachString)
-//        System.out.println(i);
-//    }
-
-
-
-
     public void dataDecripter () {
-        map = new Properties();
         for (String i : getDataEachStringWithoutComments()) {
             if      (i.contains("print")) {
-//                printDecripter(i);
+                printDecripter(i);
             }
             else if (i.contains("set")) {
                 setDecripter(i);
             }
         }
-
-
-//        if (dataEachStringForSet[1].contains("+") || dataEachStringForSet[1].contains("-")) {
-//            dataEachStringForSetEachFragment = dataEachStringForSet[1].split(" ");
-//            for (String j : dataEachStringForSetEachFragment) {
-//                for (Object k : map.keySet()) {
-//                    if (j.equals(k)) {
-//                        j = String.valueOf(map.get(k));
-//                    }
-//                }
-//            }
-//        }
-//        for (Object k : map.keySet()) {
-//            System.out.println(k);
-//        }
-
-//        System.out.println(dataEachStringForSet[0] + " = " + map.get(dataEachStringForSet[0]));
     }
 
     public void printDecripter (String string) {
-        dataEachStringForPrint = string.replace("print ", "").split("\"");
+        dataEachStringForPrint = string.replaceAll("print ", "").split("\"");
         for (int i = 0; i < dataEachStringForPrint.length; i++) {
             if (i%2 == 1) {
                 System.out.print(dataEachStringForPrint[i]);
             } else {
-                if (dataEachStringForPrint[i].contains("$")) {
-                    System.out.print(dataEachStringForPrint[i].replace(" ", "").replace(",", ""));
+                if (map.containsKey(dataEachStringForPrint[i].replaceAll(" ", "").replaceAll(",", ""))) {
+                    System.out.println(map.get(dataEachStringForPrint[i]));
                 }
             }
         }
@@ -102,33 +68,22 @@ public class Decripter {
     }
 
     public void setDecripter (String string) {
-        temp = new StringBuilder();
-        String [] dataEachStringForSet = string.replace("set ", "").split(" = ");
+        String [] dataEachStringForSet = string.replaceAll("set ", "").replaceAll(",", "").split(" = ");
         String [] dataAfterEqual = dataEachStringForSet[1].split(" ");
-//        System.out.println(dataAfterEqual.length);
-//        System.out.println(dataEachStringForSet[1]);
         if (dataAfterEqual.length == 1) {
-            map.put(dataEachStringForSet[0].replace(" ", ""), dataEachStringForSet[1].replace(" ", ""));
+            map.put(dataEachStringForSet[0].replaceAll(" ", ""), dataEachStringForSet[1].replaceAll(" ", ""));
         } else {
-            for (int i = 0; i < dataAfterEqual.length; i++) {
-                if (dataAfterEqual[i].contains("$") && map.containsKey(dataAfterEqual[i])) {
-//                    dataAfterEqual[i] = String.valueOf(map.get(dataAfterEqual[i]));
-                    temp.append(String.valueOf(map.get(dataAfterEqual[i])));
+            String temp = "";
+            for (String i : dataAfterEqual) {
+                if (map.containsKey(i)) {
+                    temp = temp + map.get(i);
                 } else {
-                    temp.append(dataAfterEqual[i]);
+                    temp = temp + i;
                 }
-//                System.out.println(map.getProperty(dataAfterEqual[i]));
-
-//                System.out.println(temp);
             }
-            System.out.println(temp);
-//            for (String i : dataAfterEqual) {
-////                System.out.println(i);
-//                temp.append(i);
-//            }
-//            System.out.println(temp);
+            map.put(dataEachStringForSet[0].replaceAll(" ", ""), temp);
+//            System.out.println(map);
         }
-
     }
 }
 
